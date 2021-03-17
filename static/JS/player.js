a34bcbeeaa4ce16cc51f66a4d33102788f699f50
domitek <portalgier@gmail.com>
2021-03-17 18:53:51 +0100

    0.1.1

--- static/JS/player.js
+++ static/JS/player.js
@@ -1,7 +1,91 @@
-function player_change(song)
-    {
-     document.getElementById("player").pause();
-     document.getElementById("player").setAttribute('src', song);
-     document.getElementById("player").load();
-     document.getElementById("player").play();
+const audioPlayer = document.querySelector(".audio-player");
+var audio = document.getElementById("player");
+
+console.dir(audio);
+
+audio.addEventListener(
+  "loadeddata",
+  () => {
+    audioPlayer.querySelector(".time .length").textContent = getTimeCodeFromNum(
+      audio.duration
+    );
+    audio.volume = .75;
+  },
+  false
+);
+
+const timeline = audioPlayer.querySelector(".timeline");
+timeline.addEventListener("click", e => {
+  const timelineWidth = window.getComputedStyle(timeline).width;
+  const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
+  audio.currentTime = timeToSeek;
+}, false);
+
+const volumeSlider = audioPlayer.querySelector(".controls .volume-slider");
+volumeSlider.addEventListener('click', e => {
+  const sliderWidth = window.getComputedStyle(volumeSlider).width;
+  const newVolume = e.offsetX / parseInt(sliderWidth);
+  audio.volume = newVolume;
+  audioPlayer.querySelector(".controls .volume-percentage").style.width = newVolume * 100 + '%';
+}, false)
+
+setInterval(() => {
+  const progressBar = audioPlayer.querySelector(".progress");
+  progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";
+  audioPlayer.querySelector(".time .current").textContent = getTimeCodeFromNum(
+    audio.currentTime
+  );
+}, 500);
+
+const playBtn = audioPlayer.querySelector(".controls .toggle-play");
+playBtn.addEventListener(
+  "click",
+  () => {
+    if (audio.paused) {
+      playBtn.classList.remove("fa-play");
+      playBtn.classList.add("fa-pause");
+      audio.play();
+    } else {
+      playBtn.classList.remove("fa-pause");
+      playBtn.classList.add("fa-play");
+      audio.pause();
     }
+  },
+  false
+);
+
+audioPlayer.querySelector(".volume-button").addEventListener("click", () => {
+  const volumeEl = audioPlayer.querySelector(".volume-container .volume");
+  audio.muted = !audio.muted;
+  if (audio.muted) {
+    volumeEl.classList.remove("fa-volume-up");
+    volumeEl.classList.add("fa-volume-mute");
+  } else {
+    volumeEl.classList.add("fa-volume-up");
+    volumeEl.classList.remove("fa-volume-mute");
+  }
+});
+
+function player_change(song){
+  audio.pause();
+  playBtn.classList.remove("fa-pause");
+  playBtn.classList.add("fa-play");
+  audio.setAttribute('src', song);
+  audio.load();
+  audio.play();
+  playBtn.classList.remove("fa-play");
+  playBtn.classList.add("fa-pause");
+}
+
+function getTimeCodeFromNum(num) {
+  let seconds = parseInt(num);
+  let minutes = parseInt(seconds / 60);
+  seconds -= minutes * 60;
+  const hours = parseInt(minutes / 60);
+  minutes -= hours * 60;
+
+  if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+  return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+    seconds % 60
+  ).padStart(2, 0)}`;
+}
