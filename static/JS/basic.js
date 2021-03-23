41782ac3185efcabcfd27d3ace12500482c6cdec
kamyrdol32 <kamyrdol32@gmail.com>
2021-03-23 19:34:08 +0100

    0.2.0

--- static/JS/basic.js
+++ static/JS/basic.js
@@ -1,16 +1,19 @@
-$(document).ready(function() {
- // executes when HTML-Document is loaded and DOM is ready
-console.log("document is ready");
+function like(ID) {
+    $.getJSON('/like/add', {
+        id: ID
+    }, function(data) {
+        console.log(data);
+        $("#ID_Like").css({"color": "green"});
+        location.reload();
+    });
+}
 
-
-// document ready
-});
-
-
-window.onload = function() {
- // executes when complete page is fully loaded, including all frames, objects and images
-console.log("window is loaded");
-
-
-// window load
-};
+function unlike(ID) {
+    $.getJSON('/unlike/add', {
+        id: ID
+    }, function(data) {
+        console.log(data);
+        $("#ID_Dislike").css({"color": "red"});
+        location.reload();
+    });
+}
